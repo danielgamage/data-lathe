162d339386ab861dae68375adc7f03157ddf8c25
Daniel Gamage <hello@danielgamage.com>
2024-07-14 23:57:50 +0000

    bulk format

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript"
 
 const config = {
-  plugins: [typescript({
-    exclude: ["docs/**/*", "node_modules/**/*", "test/**/*"],
-  })],
+  plugins: [
+    typescript({
+      exclude: ["docs/**/*", "node_modules/**/*", "test/**/*"],
+    }),
+  ],
   input: "src/index.ts",
   output: {
     dir: "./",
     format: "esm",
     compact: true,
   },
-};
+}
 
-export default config;
+export default config
